dc011f0373d654e145b2839a761cbc2498c04246
Emanuel Alcala-Arredondo <alcala.arredondo1@gmail.com>
2023-12-26 11:39:36 -0800

    Installed pygame_gui, created basic menu window with host and join buttons relative to eachother.

--- src/run_game.py
+++ src/run_game.py
@@ -1,37 +1,60 @@
 import pygame
+import pygame_gui
 
-pygame.init()
 running = True
 WIDTH, HEIGHT = 1280, 720
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-# Starts in the middle of the screen. 
-player_pos = pygame.Vector2(1280/2, 720/2)
-clock = pygame.time.Clock() # Controls FPS
+clock = pygame.time.Clock() # Controls FPS and tracks time.
+# dt = delta
+dt = 0 
 
-dt = 0 # movement modifier
+pygame.init()
+pygame.display.set_caption("Menu")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+background = pygame.Surface((WIDTH, HEIGHT))
+background.fill(pygame.Color('#a575c6'))
+
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+
+# Buttons
+host_rect = pygame.Rect((0,200), (100, 50)) # For positioning
+join_rect = pygame.Rect((0, 20), (100, 50))
+host_bt = pygame_gui.elements.UIButton(relative_rect=host_rect,
+                                       text="Host",
+                                       manager=manager,
+                                       container=None,
+                                       anchors={'centerx': 'centerx'})
+join_bt = pygame_gui.elements.UIButton(relative_rect=join_rect,
+                                       text="Join",
+                                       manager=manager,
+                                       container=None,
+                                       anchors={'centerx': 'centerx',
+                                                'top_target': host_bt})
+
 
 while running:
-    # Checks for changes in events.
+    dt = clock.tick(30) / 1000 # Limited to 30 FPS
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    SCREEN.fill("blue")
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == host_bt:
+                print("Hosting session...")
+        
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == join_bt:
+                print("Joining session...")
 
-    pygame.draw.circle(SCREEN, "black", player_pos, 35)
-    
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        manager.process_events(event)
 
-    pygame.display.flip()
+    manager.update(dt)
 
-    dt = clock.tick(60) / 1000
+    # blit is used to add a surface to the screen.
+    screen.blit(background, (0, 0))
+    manager.draw_ui(screen)
+
+    pygame.display.update() # Updates the whole screen (if no argument is passed).
 
 pygame.quit()
